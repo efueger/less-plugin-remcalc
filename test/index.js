cede0caa6784de62c68f960fac735904b601f24b
Guillaume Coguiec <guillaume.coguiec@corp.ovh.com>
2016-12-15 09:07:20 -0500

    Update the test runner logic.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-var path = require("path");
-var less = require("less");
-var tester = require("less/test/less-test")();
-var plugin = require(path.join(__dirname, "..", "lib"));
+var path = require('path');
+var less = require('less');
+var tester = require('less/test/less-test')();
+var plugin = require(path.join(__dirname, '..', 'dist', 'plugin'));
 
 var options = { strictMath: true, silent: true, plugins: [plugin] };
 
-tester.runTestSet(options, "functions/");
+tester.runTestSet(options, 'functions/');
 
 if (tester.finish) {
   test.finish();
